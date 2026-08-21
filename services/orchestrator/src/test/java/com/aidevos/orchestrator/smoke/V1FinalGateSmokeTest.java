d75465989edc15983674f34c9319cfb97349ef4f
Administrator <541124712@qq.com>
2026-08-21 12:15:04 +0800

    AI change change-5f234cb3-3f03-4754-bee8-339f92626ae4 for task task-9001ea80-5a12-4e3b-afaf-ce3d7636a1de

---
+++ services/orchestrator/src/test/java/com/aidevos/orchestrator/smoke/V1FinalGateSmokeTest.java
@@ -0,0 +1,13 @@
+package com.aidevos.orchestrator.smoke;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class V1FinalGateSmokeTest {
+
+    @Test
+    void verifiesV1FinalGate() {
+        assertEquals("AI_DEV_OS_V1", "AI_DEV_OS_V1");
+    }
+}
